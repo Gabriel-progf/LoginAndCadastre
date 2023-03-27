cead04df2e1a5ff7c36196a00b10585720224f32
Gabriel <gabriel.ol.macedo@hotmail.com>
2023-03-27 14:51:25 -0300

    Create layer model.py

---
+++ backend/model.py
@@ -0,0 +1,36 @@
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker, declarative_base
+from datetime import datetime
+
+USER= "root"
+PASSWORD= ""
+HOST= "localhost"
+PORT= "3306"
+SCHEMA= "fastApidb"
+
+CONN = f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{SCHEMA}"
+
+
+engine = create_engine(CONN, echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    email = Column(String(50))
+    password = Column(String(50))
+
+    
+class Token(Base):
+    __tablename__ = "token"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    token = Column(String(50))
+    date = Column(DateTime, default=datetime.utcnow())
+
+
+Base.metadata.create_all(engine)
